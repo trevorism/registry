254971cc986501c8903ea1aad5961656456820a9
Trevor Brooks <trevorism@gmail.com>
2017-08-21 02:44:28 -0400

    Cleanup code for deployment

--- src/main/webapp/js/registry.js
+++ src/main/webapp/js/registry.js
@@ -1,20 +1,17 @@
 $(function(){
 
     $.get( "api/service/web", function( data ) {
-
         for(var i = 0; i < data.length; i++){
             $("#serviceTableContent").append(renderRow(data[i]));
         }
-
-        alert("blah1");
     });
 
     function renderRow(item){
         var row = $("<tr />")
         row.append($("<td>" + item.id + "</td>"));
         row.append($("<td>" + item.name + "</td>"));
-        row.append($("<td>" + item.url + "</td>"));
-        row.append($("<td>" + item.giturl + "</td>"));
+        row.append($("<td><a href='" + item.url + "'>" + item.url + "</a></td>"));
+        row.append($("<td><a href='" + item.giturl + "'>" + item.giturl + "</a></td>"));
         row.append($("<td>" + true + "</td>"));
         return row;
     }
