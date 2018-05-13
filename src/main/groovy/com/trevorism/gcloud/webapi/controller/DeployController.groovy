22d84687f0f9edf62095406be866f65c2302a629
Trevor Brooks <trevorism@gmail.com>
2018-05-12 22:13:21 -0400

    Added logging statement

--- src/main/groovy/com/trevorism/gcloud/webapi/controller/DeployController.groovy
+++ src/main/groovy/com/trevorism/gcloud/webapi/controller/DeployController.groovy
@@ -5,7 +5,9 @@ import com.trevorism.data.Repository
 import com.trevorism.gcloud.webapi.controller.com.trevorism.gcloud.model.Deploy
 import com.trevorism.gcloud.webapi.controller.com.trevorism.gcloud.model.Service
 import com.trevorism.http.headers.HeadersHttpClient
+import com.trevorism.secure.Secure
 import io.swagger.annotations.Api
+import io.swagger.annotations.ApiOperation
 
 import javax.ws.rs.Consumes
 import javax.ws.rs.POST
@@ -26,16 +28,20 @@ class DeployController {
     private static final Logger log = Logger.getLogger(DeployController.class.name)
     private static final Repository<Service> repository = new PingingDatastoreRepository<>(Service)
 
+    @ApiOperation(value = "Adds the service if it is not recognized to the registry **Secure")
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
+    @Secure
     boolean storeDeploy(@Context HttpHeaders headers, Deploy deployData){
         Service service = Service.fromDeploy(deployData)
         List<Service> allServices = repository.list()
 
         def found = allServices.find{ service.name == it.name }
-        if(found || !service.name)
+        if(found || !service.name) {
+            log.fine("Found the service already or the request did not have the required information.")
             return false
+        }
 
         long max = allServices.max{it.id}.id
         service.id = max + 1
